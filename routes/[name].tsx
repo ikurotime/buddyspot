b4a2a8f3d9b492b4e989d2a89136f032a77e34fd
ikurotime <davidhuertasortiz@gmail.com>
2022-10-17 21:41:52 +0200

    🐛  Fix permanent error page

--- routes/[name].tsx
+++
@@ -1,6 +0,0 @@
-import { PageProps } from "$fresh/server.ts";
-import Layout from "../components/Layout.tsx";
-
-export default function Random(props: PageProps) {
-  return <Layout centered>Sorry! 404</Layout>;
-}
