9d9208bb3aa7bcb14093a79848bd1c38bdb7ab6c
ikurotime <davidhuertasortiz@gmail.com>
2022-10-17 21:32:55 +0200

    🐛  Fix Supabase url to import json

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js?dts";
+import { createClient } from "supabase";
 
 const supabaseUrl = Deno.env.get("SUPABASE_URL") as string;
 const supabaseKey = Deno.env.get("SUPABASE_KEY") as string;
