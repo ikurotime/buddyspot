3693516a907a9d110eb20b879c6f1af72ec8a072
ikurotime <davidhuertasortiz@gmail.com>
2022-10-17 21:47:23 +0200

    Revert "🐛  Fix permanent error page"
    This reverts commit b5162774506631ffbd8fad884077d56d297bcf2d.

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,15 +3,16 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/api/login.ts";
-import * as $1 from "./routes/api/register.ts";
-import * as $2 from "./routes/api/registered-emails.ts";
-import * as $3 from "./routes/auth/login.tsx";
-import * as $4 from "./routes/auth/register.tsx";
-import * as $5 from "./routes/home/create.tsx";
-import * as $6 from "./routes/home/index.tsx";
-import * as $7 from "./routes/index.tsx";
-import * as $8 from "./routes/room/[name].tsx";
+import * as $0 from "./routes/[name].tsx";
+import * as $1 from "./routes/api/login.ts";
+import * as $2 from "./routes/api/register.ts";
+import * as $3 from "./routes/api/registered-emails.ts";
+import * as $4 from "./routes/auth/login.tsx";
+import * as $5 from "./routes/auth/register.tsx";
+import * as $6 from "./routes/home/create.tsx";
+import * as $7 from "./routes/home/index.tsx";
+import * as $8 from "./routes/index.tsx";
+import * as $9 from "./routes/room/[name].tsx";
 import * as $$0 from "./islands/Confetti.tsx";
 import * as $$1 from "./islands/Counter.tsx";
 import * as $$2 from "./islands/HomeRooms.tsx";
@@ -21,15 +22,16 @@ import * as $$5 from "./islands/RegisteredEmail.tsx";
 
 const manifest = {
   routes: {
-    "./routes/api/login.ts": $0,
-    "./routes/api/register.ts": $1,
-    "./routes/api/registered-emails.ts": $2,
-    "./routes/auth/login.tsx": $3,
-    "./routes/auth/register.tsx": $4,
-    "./routes/home/create.tsx": $5,
-    "./routes/home/index.tsx": $6,
-    "./routes/index.tsx": $7,
-    "./routes/room/[name].tsx": $8,
+    "./routes/[name].tsx": $0,
+    "./routes/api/login.ts": $1,
+    "./routes/api/register.ts": $2,
+    "./routes/api/registered-emails.ts": $3,
+    "./routes/auth/login.tsx": $4,
+    "./routes/auth/register.tsx": $5,
+    "./routes/home/create.tsx": $6,
+    "./routes/home/index.tsx": $7,
+    "./routes/index.tsx": $8,
+    "./routes/room/[name].tsx": $9,
   },
   islands: {
     "./islands/Confetti.tsx": $$0,
