c6c608704b887bc2cab9fb3ff5e281529d0b342f
ikurotime <davidhuertasortiz@gmail.com>
2022-10-19 00:11:06 +0200

    ✨ 404 page

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,6 +1,6 @@
 import { PageProps } from "$fresh/server.ts";
 import Layout from "../components/Layout.tsx";
 
-export default function Random(props: PageProps) {
+export default function NotFound(props: PageProps) {
   return <Layout centered>Sorry! 404</Layout>;
 }
