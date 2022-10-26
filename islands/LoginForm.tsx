0c03893f21977fdb9592df652f351ee618467b51
ikurotime <davidhuertasortiz@gmail.com>
2022-10-26 18:59:51 +0200

    ✨ Login and Register Error Validation

--- islands/LoginForm.tsx
+++ islands/LoginForm.tsx
@@ -1,7 +1,8 @@
 import { useState } from "preact/hooks";
-import Button from "../components/Button.tsx";
+
 export default function LoginForm() {
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   //const { login } = useAuth();
   const [formData, setFormData] = useState({
     email: "",
@@ -16,6 +17,7 @@ export default function LoginForm() {
   };
   const handleLogin = async (e: { preventDefault: () => void }) => {
     setLoading(true);
+    setError("");
     e.preventDefault();
     try {
       //const res = await login(formData.email, formData.password);
@@ -31,10 +33,12 @@ export default function LoginForm() {
       });
 
       const json = await res.json();
-      console.log(res);
-      const { user, error } = json;
-      if (error) {
-        throw Error(error);
+      console.log(json);
+      const { user, error, status, message} = json;
+
+      if (status === 400) {
+        setError(message);
+        throw Error(message);
       }
       if (user) {
         sessionStorage.setItem("user", JSON.stringify(user));
@@ -52,7 +56,7 @@ export default function LoginForm() {
 
   return (
     <form
-      className="flex flex-col gap-3 m-3 text-black"
+      className="flex flex-col gap-3 m-3 text-white"
       onSubmit={handleLogin}
     >
       <input
@@ -60,23 +64,24 @@ export default function LoginForm() {
         onChange={onChange}
         type="text"
         placeholder="name@email.com"
-        className="py-2 px-4 rounded-md "
+        className={"py-2 px-4 rounded-md bg-gray-600" + (error ? " border border-red-500" : "")}
       />
       <input
         name="password"
         onChange={onChange}
         type="password"
         placeholder="strong-password"
-        className="py-2 px-4 rounded-md "
+        className={"py-2 px-4 rounded-md bg-gray-600" + (error ? " border border-red-500" : "")}
       />
+      {error && <p className="text-red-500">{error}</p>}
       <button
         type="submit"
         disabled={loading}
-        className="bg-green-400 p-2 rounded-md text-black"
+        className={"bg-green-400 p-2 rounded-md text-black w-24 self-end" + (loading ? " opacity-50" : "")}
       >
         {loading ? <span>loading...</span> : "Sign in"}
       </button>
-      <p className="max-w-[30ch] m-auto my-3 text-sm text-white">
+      {/* <p className="max-w-[30ch] m-auto my-3 text-sm text-white">
         - Or sign in with-
       </p>
       <div className="flex w-full justify-around gap-3  text-white">
@@ -84,18 +89,45 @@ export default function LoginForm() {
           className="bg-gray-100 text-black"
           label="Google"
           loading={loading}
+          onClick={() => {
+            fetch("/auth/login/", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify({provider: "google"})
+                });
+          }}
         />
         <Button
           className="bg-blue-400"
           label="Twitter"
           loading={loading}
+          onClick={() => {
+            fetch("/auth/login/", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify({provider: "twitter"})
+                });
+          }}
         />
         <Button
           className="bg-gray-900"
           label="Github"
           loading={loading}
+          onClick={() => {
+            fetch("/auth/login/", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify({provider: "github"})
+                });
+          }}
         />
-      </div>
+      </div> */}
     </form>
   );
 }
